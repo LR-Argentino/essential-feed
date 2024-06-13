16af331f0c0b274e831450e94ab3a4dfcb13990f
LR-Argentino <luca.argentino@outlook.com>
2024-06-13 10:39:02 +0200

    add FeedItem Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Luca Argentino on 13.06.24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
