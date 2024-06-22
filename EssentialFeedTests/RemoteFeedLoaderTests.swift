7f782f75eda3230b3cd469db8fc20c672d0bd3b3
LR-Argentino <luca.argentino@outlook.com>
2024-06-22 19:58:18 +0200

    Refactor test code

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -14,7 +14,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
         
-        XCTAssertNil(client.requestedURL)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
     func test_load_requestDataFromURL() {
@@ -23,7 +23,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         sut.load()
          
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestDataFromURL() {
@@ -44,11 +44,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
     }
     
    private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
         var requestedURLs = [URL]()
 
         func get(from url: URL) {
-            requestedURL = url
             requestedURLs.append(url)
         }
     }
