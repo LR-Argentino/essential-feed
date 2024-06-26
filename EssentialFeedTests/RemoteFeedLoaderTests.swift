7eb86500bea951250894fa21e2d1aa95d2b31436
LR-Argentino <luca.argentino@outlook.com>
2024-06-26 18:28:42 +0200

    Detailed description of which function the test failed when using the helpfer function 'expect'

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -73,13 +73,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    func expect(_ sut: RemoteFeedLoader, toCompleteWithError error: RemoteFeedLoader.Error, when action: () -> Void) {
+    func expect(_ sut: RemoteFeedLoader, toCompleteWithError error: RemoteFeedLoader.Error, when action: () -> Void,  file: StaticString = #filePath, line: UInt = #line) {
         var capturedError = [RemoteFeedLoader.Error]()
         sut.load { capturedError.append($0) }
         
         action()
         
-        XCTAssertEqual(capturedError, [error])
+        XCTAssertEqual(capturedError, [error], file: file, line: line)
     }
     
     private class HTTPClientSpy: HTTPClient {
