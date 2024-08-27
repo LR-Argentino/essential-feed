953a63ca9287da5923a395310361585eedc7e988
LR-Argentino <luca.argentino@outlook.com>
2024-08-27 10:41:00 +0200

    save command does not request cache insertion on deletion error

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -10,9 +10,14 @@ import EssentialFeed
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    var insertCallCount = 0
     
     func deleteCachedFeed() {
         deleteCachedFeedCallCount += 1
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        
     }
 }
 
@@ -26,6 +31,8 @@ class LocalFeedLoader {
     func save(_ items: [FeedItem]) {
         store.deleteCachedFeed()
     }
+    
+
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -35,7 +42,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
-
+    
     func test_save_requestsCacheDeletion() {
         let (sut, store) = makeSUT()
         let items = [uniqueItems(), uniqueItems()]
@@ -45,15 +52,34 @@ final class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
     
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let (sut, store) = makeSUT()
+        let items = [uniqueItems(), uniqueItems()]
+        let deletionError = anyError()
+        
+        sut.save(items)
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
     // MARK: - Helpers
     private func uniqueItems() -> FeedItem {
         return FeedItem(id: UUID(), description: "any description", location: "any location", imageURL: URL(string: "www.any-image.com")!)
     }
     
-    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    private func makeSUT(file: StaticString = #filePath,
+                         line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store)
+        trackForMemoryLeaks(sut)
         
         return (sut, store)
+    }
+    
+    private func anyError() -> Error {
+        let error = NSError(domain: "any", code: 0, userInfo: nil)
+        return error
     }
 }
