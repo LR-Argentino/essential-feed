cfc3a777f78cf5f0470e3100244f373446bc22dc
LR-Argentino <luca.argentino@outlook.com>
2024-09-27 22:34:22 +0200

    Make CodableFeedStore queue concurrent to allow multiple retrieve to be proceeded in parallel (since it has no side-effects) and use `barriers` when performing side-effects to gurantee data consistency and avoid race condition

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class CodableFeedStore {
+class CodableFeedStore: FeedStore {
     private struct Cache: Codable {
         let feed: [CodableImageFeed]
         let timestamp: Date
@@ -18,6 +18,8 @@ class CodableFeedStore {
         }
     }
     private let storeURL: URL
+    private let queue = DispatchQueue(label: "\(CodableFeedStore.self)Queue", qos: .userInitiated, attributes: .concurrent)
+    
     
     init(storeURL: URL) {
         self.storeURL = storeURL
@@ -43,28 +45,53 @@ class CodableFeedStore {
     
     
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        guard let data = try? Data(contentsOf: storeURL) else {
-            return completion(.empty)
+        // To avoid self.storeURL in Dispatch
+        let storeURL = self.storeURL
+        // retrieve has no sied effect
+        queue.async {
+            guard let data = try? Data(contentsOf: storeURL) else {
+                return completion(.empty)
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let cache = try decoder.decode(Cache.self, from: data)
+                completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+            } catch {
+                completion(.failure(error))
+            }
         }
-        
-        do {
-            let decoder = JSONDecoder()
-            let cache = try decoder.decode(Cache.self, from: data)
-            completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
-        } catch {
-            completion(.failure(error))
-        }
-     
     }
-    
+
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: feed.map(CodableImageFeed.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
-        
-        completion(nil)
+        let storeURL = self.storeURL
+        queue.async(flags: .barrier) {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableImageFeed.init), timestamp: timestamp)
+            let encoded = try! encoder.encode(cache)
+            try! encoded.write(to: storeURL)
+            
+            completion(nil)
+        }
+    }
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        let storeURL = self.storeURL
+        queue.async(flags: .barrier) {
+            guard FileManager.default.fileExists(atPath: storeURL.path) else {
+                return completion(nil)
+            }
+            
+            do {
+                try FileManager.default.removeItem(at: storeURL)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+            
+            completion(nil)
+        }
     }
 
 }
