314786c19b68715b52618aa6991ec58f4b155cfb
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-03-02 10:51:16 +0100

    Initial commit

---
+++ Prototype/Prototype/.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Luca Argentino on 02.03.2025.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
