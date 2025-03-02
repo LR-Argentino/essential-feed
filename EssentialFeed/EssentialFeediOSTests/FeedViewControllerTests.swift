c9c0f37237880445e5f12a23451e49aefeefdd20
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-03-02 12:01:21 +0100

    Add first FeedViewController Test

---
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Luca Argentino on 02.03.2025.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+final class FeedViewController {
+    init(loader: FeedViewControllerTests.LoaderSpy) {
+        
+    }
+}
+
+final class FeedViewControllerTests: XCTestCase {
+
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        let _ = FeedViewController(loader: loader)
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+    }
+
+    
+    class LoaderSpy {
+        private(set) var loadCallCount: Int = 0
+    }
+}
